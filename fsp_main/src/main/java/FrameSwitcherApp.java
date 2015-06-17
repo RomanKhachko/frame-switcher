075f803473c13b106a1a99f7a9ec80caac191bc0
Roman Khachko <romankhachko@gmail.com>
2015-06-17 23:08:36 +0300

    experimenting with spring AOP and Aspectj

--- fsp_main/src/main/java/FrameSwitcherApp.java
+++ fsp_main/src/main/java/FrameSwitcherApp.java
@@ -1,3 +1,9 @@
+import com.rk.fsp.VerEntity;
+import com.rk.fsp.Verifier;
+import com.rk.fsp.annotations.RequireSwitchingToFrame;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Created by roman on 6/1/15.
  */
@@ -5,9 +11,28 @@
 //@EnableAspectJAutoProxy
 public class FrameSwitcherApp {
 
+    private ApplicationContext context;
+
+    private static FrameSwitcherApp frameSwitcherApp;
+
+    private FrameSwitcherApp(){
+        context =  new ClassPathXmlApplicationContext("frame-switcher.xml");
+    }
+
     public static void init(){
+        if(frameSwitcherApp == null){
+            frameSwitcherApp = new FrameSwitcherApp();
+        }
         //TODO: initialize spring context
 
         // TODO: try additional initialization, not trough xml
+    }
+
+    public static void main(String[] args) {
+        FrameSwitcherApp.init();
+//        frameSwitcherApp.context.getBean("verifier", Verifier.class).foo().bar();
+        new VerEntity().bar();
+//        new Verifier().foo();
+
     }
 }
--- fsp_main/src/main/java/com/rk/fsp/aspects/SwitchToFrameAspect.java
+++ fsp_main/src/main/java/com/rk/fsp/aspects/SwitchToFrameAspect.java
@@ -2,9 +2,11 @@ package com.rk.fsp.aspects;
 
 import com.rk.fsp.annotations.RequireSwitchingToFrame;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.beans.factory.annotation.Configurable;
 
 /**
  * Created by roman on 5/30/15.
@@ -13,16 +15,29 @@ import org.aspectj.lang.annotation.Pointcut;
 public class SwitchToFrameAspect {
 
     @Pointcut("@annotation(com.rk.fsp.annotations.RequireSwitchingToFrame)")
-    private void whenMethodIsAnnotatedByFrameSwitcher() {}
+    private void whenMethodIsAnnotatedByFrameSwitcher() {
+    }
 
-    @Before("whenMethodIsAnnotatedByFrameSwitcher")
-    public void switchFrameBefore(/*JoinPoint joinPoint,*/ RequireSwitchingToFrame annotation) {
-        System.out.println("Locator type: " + annotation.locatorType());
+//    @Pointcut("@annotation(requireSwitchingToFrame)")
+//    private void whenMethodIsAnnotatedByFrameSwitcher(RequireSwitchingToFrame requireSwitchingToFrame) {
+//        System.out.println("in pointcut");
+//    }
+
+//    @Pointcut("within(com.rk.fsp.Verifier.*)")
+//    private void whenMethodIsAnnotatedByFrameSwitcher() {}
+
+    @Before("whenMethodIsAnnotatedByFrameSwitcher()")
+    public void switchFrameBefore(JoinPoint joinPoint /*RequireSwitchingToFrame annotation*/) {
+//        System.out.println("Locator type: " + annotation.locatorType());
 //        joinPoint.getClass().getAnnotations();
+        System.out.println("join point: " + joinPoint.getSignature());
         // TODO: switch to frame
     }
 
-    public void switchToDefaultContent(){
+    //    @After("com.rk.fsp.aspects.SwitchToFrameAspect.whenMethodIsAnnotatedByFrameSwitcher")
+    @After("whenMethodIsAnnotatedByFrameSwitcher()")
+    public void switchToDefaultContent() {
+        System.out.println("in after");
         // TODO: switch to default content
     }
 }
